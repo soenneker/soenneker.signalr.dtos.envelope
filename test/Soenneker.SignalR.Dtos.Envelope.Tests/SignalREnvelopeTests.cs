0d4c41461aaa6c3022fd42b7411aa7dfde0c8b79
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:42 -0500

    TUnit conversion

--- test/Soenneker.SignalR.Dtos.Envelope.Tests/SignalREnvelopeTests.cs
+++ test/Soenneker.SignalR.Dtos.Envelope.Tests/SignalREnvelopeTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.SignalR.Dtos.Envelope.Tests;
 
 public sealed class SignalREnvelopeTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
